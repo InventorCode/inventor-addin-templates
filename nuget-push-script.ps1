06a05a506d34a8a1ade9e609c2ccb8030b49a4ee
jordanrobot <119146+jordanrobot@users.noreply.github.com>
2021-11-21 18:47:21 -0500

    chore: update nuget push script

--- nuget-push-script.ps1
+++ nuget-push-script.ps1
@@ -1,2 +1,1 @@
-$ApiKey = Read-Host -Prompt "Enter the Nuget API key:"
-nuget push nupkg/InventorCode.AddinTemplates.0.3.0.nupkg $ApiKey -Source https://api.nuget.org/v3/index.json
+nuget push nupkg/InventorCode.AddinTemplates.0.3.0.nupkg -Source https://api.nuget.org/v3/index.json
